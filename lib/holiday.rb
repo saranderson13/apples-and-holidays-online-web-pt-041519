5ea439b1e0994e0f6b5cac7b81738eff8fde5559
Sarah Anderson <saranderson13@gmail.com>
2019-04-27 17:57:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
-      binding.pry
+      holiday if supplies.include?("BBQ")
     end
   end
 end
