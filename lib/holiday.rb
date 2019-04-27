cbf0da9fa2dbab8102458056987c5170452f8b74
Sarah Anderson <saranderson13@gmail.com>
2019-04-27 17:50:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,9 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holidays| 
     puts "#{season.to_s.capitalize}:"
-    
+    holidays.each do |holiday, supplies|
+      binding.pry
+    end
   end
 end
 
