373f0900c00a5757784ffbaeb209ceaec16df5fc
Sarah Anderson <saranderson13@gmail.com>
2019-04-27 17:59:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,10 +73,11 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.each do |season, holidays|
+  holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
     end
+    self.flatten.compact
   end
 end
 
